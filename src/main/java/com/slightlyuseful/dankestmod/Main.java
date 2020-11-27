0cab67e819e72d07624bf4e2294e648395371953
SkilzMastr <64101555+SkilzMastr@users.noreply.github.com>
2020-11-26 23:50:10 -0500

    Cleanup 1

--- src/main/java/com/slightlyuseful/dankestmod/Main.java
+++ src/main/java/com/slightlyuseful/dankestmod/Main.java
@@ -64,21 +64,5 @@ public class Main
                 map(m->m.getMessageSupplier().get()).
                 collect(Collectors.toList()));
     }
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
-    }
 }
+
